8454d1fccd222561eb03722c1488daf92917e31d
mojohn89 <mojohnsen89@gmail.com>
2018-05-24 08:46:20 +0200

    Create RebootAutomation.cs
    Main with password config and command loop, namespace MachineRebootAutomation

---
+++ RebootAutomation.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MachineRebootAutomation {
+    class RebootAutomationMain {
+        static void Main(string[] args) {
+
+            /*** MAIL SERVER CONFIG AND SETUP ***/
+            string mailHost = "10.10.10.10";
+            int mailPort = 25;
+            string mailDomain = @"@domain.com";
+            MailHandler mailer = new MailHandler(mailHost, mailPort, mailDomain);
+
+            /*** SQL CONFIG AND SETUP ***/
+            string SQLserver = "11.11.11.11";
+            string SQLdatabase = "Monitoring";
+            string SQLuser = "sql_username";
+            string SQLpass = "sql_password";
+            SQLHandler SQL = new SQLHandler(SQLserver, SQLdatabase, SQLuser, SQLpass);
+
+            /*** PSTOOLS CONFIG AND SETUP ***/
+            string TVMuser = "machine_username";
+            string TVMpass = "machine_password";
+            PSToolsHandler PSTools = new PSToolsHandler(TVMuser, TVMpass, mailer);
+
+
+           //SQL query to get machine data
+            string queryMachine = "SELECT Site, Machine, DomainAddress FROM DeviceStatus WHERE EquipmentType = 2";
+            DataTable SQLresult = SQL.GetData(queryMachine);
+                       
+            // Example case for main sequence with parallell processing
+            Parallel.ForEach(SQLresult.AsEnumerable(), Machine => {
+            
+                //Retrieving variables from datatable
+                string EquipmentIPAddress = Machine.Field<string>("DomainAddress");
+                string SiteName = Machine.Field<string>("Site");
+                string MachineName = Machine.Field<string>("Machine");
+
+                PSTools.DeleteEODfilesAndReboot(DomainAddress);
+
+            }); 
+
+            // For user input before closing
+            Console.ReadLine();
+        }
+    }
+}
